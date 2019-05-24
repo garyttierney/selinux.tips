2ed4686726c9d4b936de97bf508925f4022d49e3
Gary Tierney <gary.tierney@fastmail.com>
2019-05-24 22:55:23 +0100

    Fix tabbed-code directive under new Sphinx version

--- source/exts/sphinxext/tabbed_code_block.py
+++ source/exts/sphinxext/tabbed_code_block.py
@@ -2,45 +2,67 @@ import os
 import re
 
 from docutils.parsers.rst import Directive, directives
+from docutils.statemachine import StringList
 from docutils import nodes
 
-from sphinx.directives import container_wrapper
+
+def container_wrapper(directive, literal_node, caption):
+    # type: (SphinxDirective, nodes.Node, str) -> nodes.container
+    container_node = nodes.container(
+        "", literal_block=True, classes=["literal-block-wrapper"]
+    )
+    parsed = nodes.Element()
+    directive.state.nested_parse(
+        StringList([caption], source=""), directive.content_offset, parsed
+    )
+    if isinstance(parsed[0], nodes.system_message):
+        msg = "Invalid caption: %s" % parsed[0].astext()
+        raise ValueError(msg)
+    elif isinstance(parsed[0], nodes.Element):
+        caption_node = nodes.caption(parsed[0].rawsource, "", *parsed[0].children)
+        caption_node.source = literal_node.source
+        caption_node.line = literal_node.line
+        container_node += caption_node
+        container_node += literal_node
+        return container_node
+    else:
+        raise RuntimeError  # never reached
+
 
 class TabbedCodeBlockDirective(Directive):
-	has_content = True
-	option_spec = {
-		'caption': directives.unchanged_required
-	}
+    has_content = True
+    option_spec = {"caption": directives.unchanged_required}
 
-	def run(self):
-		text = '\n'.join(self.content)
-		dummy_node = nodes.container(text)
+    def run(self):
+        text = "\n".join(self.content)
+        dummy_node = nodes.container(text)
 
-		self.state.nested_parse(self.content, self.content_offset, dummy_node)
+        self.state.nested_parse(self.content, self.content_offset, dummy_node)
 
-		container = nodes.container('')
-		container['classes'].append('tabbed-code-block-container')
+        container = nodes.container("")
+        container["classes"].append("tabbed-code-block-container")
 
-		for code_block in dummy_node:
-			code_block_container = nodes.container(code_block.astext())
-			code_block_container['classes'].append('tabbed-code-block')
+        for code_block in dummy_node:
+            code_block_container = nodes.container(code_block.astext())
+            code_block_container["classes"].append("tabbed-code-block")
 
-			language = re.sub(r"[\-_]", " ", code_block['language'])
-			code_block_label = nodes.strong(text=language)
-			code_block_label.set_class('language-label')
+            language = re.sub(r"[\-_]", " ", code_block["language"])
+            code_block_label = nodes.strong(text=language)
+            code_block_label.set_class("language-label")
 
-			code_block_container.append(code_block_label)
-			code_block_container.append(code_block)
+            code_block_container.append(code_block_label)
+            code_block_container.append(code_block)
 
-			container.append(code_block_container)
+            container.append(code_block_container)
 
-		self.add_name(container)
+        self.add_name(container)
 
-		caption = self.options.get('caption')
-		if caption:
-			return [container_wrapper(self, container, caption)]
+        caption = self.options.get("caption")
+        if caption:
+            return [container_wrapper(self, container, caption)]
 
-		return [container]
+        return [container]
+
 
 def setup(app):
-	app.add_directive('tabbed-code-block', TabbedCodeBlockDirective)
+    app.add_directive("tabbed-code-block", TabbedCodeBlockDirective)
